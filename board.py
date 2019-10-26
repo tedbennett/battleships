629344d29c89d6ec3c4800a60405efbacc59adc0
ted <teddsbennett@gmail.com>
2019-10-26 11:30:50 +0100

    added placement phase

--- board.py
+++ board.py
@@ -1,15 +1,10 @@
 import pygame
-
-SCREEN_WIDTH, SCREEN_HEIGHT = 400, 400
-WHITE = (255, 255, 255)
-BLACK = (0, 0, 0)
-LIGHTGREY = (100,100,100)
-DARKGREY = (50, 50, 50)
+from ship import Ship
+from constant import SCREEN_WIDTH, SCREEN_HEIGHT, WHITE, BLACK, LIGHTGREY, DARKGREY
 
 
 class Board:
-
-    def draw_board(self, screen, pieces):
+    def draw_board(self, screen, ships):
         screen.fill(DARKGREY)
         for x in range(1, 11):
             pygame.draw.line(screen, LIGHTGREY,
@@ -18,9 +13,8 @@ class Board:
             pygame.draw.line(screen, LIGHTGREY,
                              (0, SCREEN_HEIGHT * x / 10),
                              (SCREEN_WIDTH, SCREEN_HEIGHT * x / 10), 1)
-
-        for piece in pieces:
-            pygame.draw.rect(screen, WHITE, piece.get_rect())
+        for ship in ships:
+            ship.draw_ship(screen)
 
 
 FPS = 30
@@ -28,14 +22,37 @@ pygame.init()
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("Battleship")
 running = True
-pieces = []
 board = Board()
-pieces.append(Piece(1, 1))
-pieces.append(Piece(5, 4))
+ships = [Ship((0, 0), (5, 0), WHITE),
+         Ship((0, 0), (4, 0), BLACK),
+         Ship((0, 0), (3, 0), BLACK),
+         Ship((0, 0), (3, 0), BLACK),
+         Ship((0, 0), (2, 0), BLACK)]
+ship_index = 0
+movement_phase = True
 while running:
-    board.draw_board(screen, pieces)
+    board.draw_board(screen, ships)
     pygame.display.flip()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        if event.type == pygame.KEYDOWN and movement_phase:
+            if event.key == pygame.K_r:
+                ships[ship_index].rotate()
+            if event.key == pygame.K_UP:
+                ships[ship_index].translate((0, -1))
+            if event.key == pygame.K_DOWN:
+                ships[ship_index].translate((0, 1))
+            if event.key == pygame.K_LEFT:
+                ships[ship_index].translate((-1, 0))
+            if event.key == pygame.K_RIGHT:
+                ships[ship_index].translate((1, 0))
+            if event.key == pygame.K_RETURN:
+                if not ships[ship_index].check_collision(ships):
+                    ships[ship_index].set_phase("placed")
+                    if ship_index < 4:
+                        ship_index += 1
+                        ships[ship_index].set_phase("moving")
+                    else:
+                        movement_phase = False
 pygame.quit()
--- ship.py
+++ ship.py
@@ -1,39 +1,92 @@
 import pygame
-from board import SCREEN_WIDTH, SCREEN_HEIGHT, WHITE
+from constant import SCREEN_WIDTH, SCREEN_HEIGHT, WHITE, BLACK, LIGHTGREY
 
 
 class Ship:
-    def __init__(self, start, end):
+    def __init__(self, start, end, colour):
         self._start = start
         self._end = end
+        self._colour = colour
+        self._phase = "not placed"
         self._array = []
-        self.create_array()
+        self.create_array(self._start, self._end)
         self._size = len(self._array)
 
-    def create_array(self):
-        x = self._start[0]
-        y = self._start[1]
-
-        if self._start[0] == self._end[0]:
-            for y in range(self._start[1], self._end[1] + 1):
+    def create_array(self, start, end):
+        self._array = []
+        x, y = start
+        if start[0] == end[0]:
+            for y in range(start[1], end[1] + 1):
                 self._array.append((x, y))
 
-        elif self._start[1] == self._end[1]:
-            for x in range(self._start[0], self._end[0] + 1):
+        elif start[1] == end[1]:
+            for x in range(start[0], end[0] + 1):
                 self._array.append((x, y))
 
-    def draw_piece(self, screen):
+    def draw_ship(self, screen):
         for position in self._array:
-            pygame.draw.rect(screen, WHITE, self.get_rect(position))
+            if self._colour != BLACK:
+                pygame.draw.rect(screen, self._colour, self.get_rect(position))
 
-    def get_rect(self, x, y):
-        return pygame.Rect(x * SCREEN_WIDTH / 10,
-                           y * SCREEN_HEIGHT / 10,
+    def get_rect(self, position):
+        return pygame.Rect(position[0] * SCREEN_WIDTH / 10,
+                           position[1] * SCREEN_HEIGHT / 10,
                            SCREEN_WIDTH / 10,
                            SCREEN_HEIGHT / 10)
 
+    def translate(self, change):
+        temp_start = (change[0] + self._start[0], change[1] + self._start[1])
+        temp_end = (change[0] + self._end[0], change[1] + self._end[1])
+        if not self._check_boundaries(temp_start, temp_end):
+            return
+        self.create_array(temp_start, temp_end)
+        self._start, self._end = temp_start, temp_end
+
+    def rotate(self):
+        change = (self._end[0] - self._start[0], self._end[1] - self._start[1])
+        temp_end = (change[1] + self._start[0], change[0] + self._start[1])
+        if not self._check_boundaries(self._start, temp_end):
+            return
+        self.create_array(self._start, temp_end)
+        self._end = temp_end
+
     def get_size(self):
         return self._size
+
+    def _set_colour(self, colour):
+        self._colour = colour
+
+    def set_phase(self, phase):
+        self._phase = phase
+        if self._phase == "not placed":
+            self._set_colour(BLACK)
+        if self._phase == "moving":
+            self._set_colour(WHITE)
+        if self._phase == "placed":
+            self._set_colour(LIGHTGREY)
+
+    def _check_boundaries(self, start, end):
+        if (start[0] < 0 or start[0] > 9
+                or start[1] < 0 or start[1] > 9
+                or end[0] < 0 or end[0] > 9
+                or end[1] < 0 or end[1] > 9):
+            return False
+        else:
+            return True
+
+    def check_collision(self, ships):
+        for ship in ships:
+            if ship.get_phase() == "placed":
+                for position in self.get_array():
+                    if position in ship.get_array():
+                        return True
+        return False
+
+    def get_array(self):
+        return self._array
+
+    def get_phase(self):
+        return self._phase
 
     def __getitem__(self, item):
         return self._array[item]
